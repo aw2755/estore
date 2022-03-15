85195229fab36d0756e45ff8a1e0662501a29ebe
Alan Wang <aw2755@rit.edu>
2022-03-15 13:57:12 -0400

    template for the front end

--- estore-ui/fruit-store/src/app/app.module.ts
+++ estore-ui/fruit-store/src/app/app.module.ts
@@ -1,21 +1,32 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { ProductService } from './product.service';
-import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductSearchComponent } from './product-search/product-search.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
   imports: [
     BrowserModule,
+    FormsModule,
+    AppRoutingModule,
     HttpClientModule,
-    FormsModule
   ],
-  providers: [ProductService],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    ProductsComponent,
+    ProductDetailComponent,
+    MessagesComponent,
+    ProductSearchComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- estore-ui/fruit-store/src/app/product.service.ts
+++ estore-ui/fruit-store/src/app/product.service.ts
@@ -1,30 +1,125 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
 import { Product } from './product';
-import { HttpClient } from '@angular/common/http'
+import { MessageService } from './message.service';
 import { environment } from 'src/environments/environment';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable({ providedIn: 'root' })
 export class ProductService {
-  private apiServerUrl = environment.apiBaseUrl;
 
-  constructor(private http: HttpClient) { }
+  private productsUrl = environment.apiBaseUrl;  // URL to web api
 
-  public getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(`${this.apiServerUrl}/inventory/all`);
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  /** GET products from the server */
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productsUrl)
+      .pipe(
+        tap(_ => this.log('fetched products')),
+        catchError(this.handleError<Product[]>('getProducts', []))
+      );
   }
 
-  public addProduct(product: Product): Observable<Product> {
-    return this.http.post<Product>(`${this.apiServerUrl}/inventory/add`, product);
+  /** GET product by id. Return `undefined` when id not found */
+  getProductNo404<Data>(name : String): Observable<Product> {
+    const url = `${this.productsUrl}/?name=${name}`;
+    return this.http.get<Product[]>(url)
+      .pipe(
+        map(products => products[0]), // returns a {0|1} element array
+        tap(h => {
+          const outcome = h ? 'fetched' : 'did not find';
+          this.log(`${outcome} product name=${name}`);
+        }),
+        catchError(this.handleError<Product>(`getProduct name=${name}`))
+      );
   }
 
-  public updateProduct(product: Product): Observable<Product> {
-    return this.http.put<Product>(`${this.apiServerUrl}/inventory/update`, product);
+  /** GET product by name. Will 404 if id not found */
+  getProduct(name: String): Observable<Product> {
+    const url = `${this.productsUrl}/${name}`;
+    return this.http.get<Product>(url).pipe(
+      tap(_ => this.log(`fetched product name=${name}`)),
+      catchError(this.handleError<Product>(`getProduct name=${name}`))
+    );
   }
 
-  public deleteProduct(productName: String): Observable<void> {
-    return this.http.delete<void>(`${this.apiServerUrl}/inventory/delete/${productName}`);
+  /* GET products whose name contains search term */
+  searchProducts(term: String): Observable<Product[]> {
+    if (!term.trim()) {
+      // if not search term, return empty product array.
+      return of([]);
+    }
+    return this.http.get<Product[]>(`${this.productsUrl}/?name=${term}`).pipe(
+      tap(x => x.length ?
+         this.log(`found products matching "${term}"`) :
+         this.log(`no products matching "${term}"`)),
+      catchError(this.handleError<Product[]>('searchProducts', []))
+    );
+  }
+
+  //////// Save methods //////////
+
+  /** POST: add a new product to the server */
+  addProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.productsUrl, product, this.httpOptions).pipe(
+      tap((newProduct: Product) => this.log(`added product w/ name=${newProduct.name}`)),
+      catchError(this.handleError<Product>('addroduct'))
+    );
+  }
+
+  /** DELETE: delete the product from the server */
+  deleteProduct(name: String): Observable<Product> {
+    const url = `${this.productsUrl}/${name}`;
+
+    return this.http.delete<Product>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted product name=${name}`)),
+      catchError(this.handleError<Product>('deleteProduct'))
+    );
+  }
+
+  /** PUT: update the product on the server */
+  updateProduct(product: Product): Observable<any> {
+    return this.http.put(this.productsUrl, product, this.httpOptions).pipe(
+      tap(_ => this.log(`updated product price=${product.price}`)),
+      tap(_ => this.log(`updated product quantity=${product.quantity}`)),
+      catchError(this.handleError<any>('updateProduct'))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a ProductService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`ProductService: ${message}`);
   }
 }
