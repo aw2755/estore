78c874b48598434120495fdf694cd7e43dad6aec
tt1336 <78926836+tt1336@users.noreply.github.com>
2022-03-29 23:53:38 -0400

    Added some stuff

--- estore-ui/fruit-store/src/app/user.ts
+++ estore-ui/fruit-store/src/app/user.ts
@@ -2,10 +2,6 @@ import { Product } from "./product";
 
 export interface User 
 {
-<<<<<<< HEAD
     username: string; 
     shoppingCat : Product[];
-=======
-    username: string;   
->>>>>>> 8a8b0d79128ac4b5562bbca7d2b2eeafa8297940
 }
