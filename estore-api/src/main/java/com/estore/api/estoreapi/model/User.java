c233cf92f6bff2bb73a3bf782944832f690d0817
Alan Wang <aw2755@rit.edu>
2022-03-29 21:04:02 -0400

    t

--- estore-api/src/main/java/com/estore/api/estoreapi/model/User.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/User.java
@@ -16,13 +16,12 @@ public class User {
     static final String STRING_FORMAT = "User [id=%d, username=%s]";
 
     @JsonProperty("username") private String userName;
-    public ArrayList<Product> cart;
+    private ArrayList<Product> cart;
 
     public User(@JsonProperty("username") String userName) {
         this.userName = userName;
         this.cart = new ArrayList<>();
     }
-
 
     public String getUserName() {
         return userName;
